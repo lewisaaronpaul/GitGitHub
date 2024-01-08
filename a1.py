4885dc90c07aece897e650ff177eaecf70154e26
Aaron P Lewis <lewisaaronpaul@gmail.com>
2024-01-07 23:25:09 -0600

    finish (100%) coding a1 at office

--- a1.py
+++ a1.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-developing a1 feature (50% completion) at office
+"""developing a1 feature (100% completion) at office
 The correlation between reading comprehension raw score (M=160, SD=30) and vocabulary raw score (M=100, SD=15) is 0.75.
 a.	If both reading comprehension score and vocabulary score are standardized into Z score, what is their correlation?
 b.	Suppose that we want to predict reading raw score based on vocabulary raw score, what are the slope and intercept?
@@ -9,4 +8,4 @@ a.	If both reading comprehension score and vocabulary score are standardized int
 b.	Suppose that we want to predict reading raw score based on vocabulary raw score, what are the slope and intercept?
 c.	What are the slope and intercept based on standardized score?
 d.	Nora scored 120 points (raw score) on the vocabulary test, what is her predicted reading (raw score)?predicted reading (raw score)?
-
+"""
